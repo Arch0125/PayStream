deb33f9dfa5f0f2c07703189250b5d1210388c8f
Arch0125 <dasarchisman25@gmail.com>
2022-03-15 09:59:57 +0530

    :sparkles: Add Home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar';
 import Details from './Details/Details';
 import Wallet from './Wallet/Wallet';
 import Organizer from './Organizer';
+import Home from './Home';
 
 class App extends Component {
     state = {  } 
