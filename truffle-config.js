680462abba7c6ed1871861936dbda68e609738d0
Arch0125 <dasarchisman25@gmail.com>
2022-03-17 21:19:54 +0530

    src : Organizer portal revamping

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,10 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 const path = require("path");
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
+  // See <http://truffleconst HDWalletProvider = require('@truffle/hdwallet-provider');framework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "src/contracts"),
   networks: {
@@ -9,6 +12,13 @@ module.exports = {
       host: "localhost",
       port: 7545,
       network_id: "*"
-    }
+    },
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.maticvigil.com`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
   }
 };
